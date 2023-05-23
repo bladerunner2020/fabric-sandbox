3d569e60cfb70c1f99c5a2aebae181c2524a8137
bladerunnaer2020 <pivovarov@gmail.com>
2023-05-23 13:56:30 +0300

    wip

---
+++ src/MyGroup.ts
@@ -0,0 +1,36 @@
+import { Group as FabricGroup, Object as FabricObject } from 'fabric';
+
+type FabricGroupParams = ConstructorParameters<typeof FabricGroup>;
+type GroupParams = FabricGroupParams[1];
+type ExtendedGroupParams = GroupParams & {
+  x?: number;
+  y?: number;
+};
+
+class ExtendedGroup extends FabricGroup {
+  x?: number;
+  y?: number;
+
+  constructor(objects?: FabricGroupParams[0], options?: ExtendedGroupParams, objectsRelativeToGroup?: boolean) {
+    const { x, y, ...rest } = options || {};
+    super(objects, rest, objectsRelativeToGroup);
+    // Сохраняем начальные позиции группы
+    this.x = x || options?.left;
+    this.y = y || options?.top;
+  }
+
+  _onObjectAdded(obj: FabricObject): void {
+    const x = this.x || 0;
+    const y = this.y || 0;
+    obj.set({
+      left: x + obj.left,
+      top: y + obj.top
+    });
+    obj.setCoords();
+
+    // eslint-disable-next-line no-underscore-dangle
+    super._onObjectAdded(obj);
+  }
+}
+
+export default ExtendedGroup;
--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,60 @@
-import { Canvas, Text, Group }  from 'fabric';
+import {
+  Canvas, Text, Group, Rect
+}  from 'fabric';
 import './styles.css';
+import MyGroup from './MyGroup';
 
-const canvas = new Canvas('canvas', {
+const canvas = new Canvas('canvas1', {
   backgroundColor: '#F8F8F8',
   width: 800,
   height: 600,
 });
 
-const hello = new Text('Hello', { left: 75, top: 0 });
+const hello = new Text('Hello', { left: 0, top: 0 });
+console.log(hello.left)
 const world = new Text('World', { left: 150, top: 50 });
 
-const hello2 = new Text('Hello', { left: 75, top: 0 });
-const world2 = new Text('World', { left: 150, top: 50 });
+const hello2 = new Text('Hello2', { left: 0, top: 0 });
+const world2 = new Text('World2', { left: 150, top: 150 });
 
-const group = new Group([], {
-  left: 200,
-  top: 250,
-  backgroundColor: '#add8e6',
-  // objectsRelativeToGroup: true
+const group = new MyGroup([], {
+  // layout: 'fixed',
+  left: 100,
+  top: 100,
+  backgroundColor: '#add8e6'
 });
-console.log('Group', group.left, group.top, group.width, group.height);
 
-const group2 = new Group([hello2, world2], {
-  left: 200,
-  top: 250,
-  backgroundColor: '#90EE90'
-});
-console.log('Group2', group2.left, group2.top, group2.width, group2.height);
-
+const subgroup = new MyGroup([], {
+  left: 250, 
+  top: 250, 
+  backgroundColor: '#32CD32'
+})
 
 canvas.add(group);
-canvas.add(group2);
 
-group.add(hello);
-group.add(world);
 
-console.log('Group', group.left, group.top, group.width, group.height);
+setTimeout(() => {
+  group.add(hello);
+  canvas.renderAll();
+}, 1000)
+
+setTimeout(() => {
+  group.add(world);
+  canvas.renderAll();
+}, 2000)
+
+
+setTimeout(() => {
+  group.add(subgroup);
+  canvas.renderAll();
+}, 3000)
+
+setTimeout(() => {
+  subgroup.add(hello2);
+  canvas.renderAll();
+}, 4000)
+
+setTimeout(() => {
+  subgroup.add(world2);
+  canvas.renderAll();
+}, 5000)
