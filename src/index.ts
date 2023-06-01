78e9d33744d5d414f204d5ef3d84dbed573f5452
ShaMan123 <shacharnen@gmail.com>
2023-06-01 09:52:31 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,43 @@
-import {
-  Canvas, Group, Point, Line, Circle
-}  from 'fabric';
-import './styles.css';
+import { Canvas, Group, Point, Line, Circle } from "fabric";
+import "./styles.css";
 
 // We have a React Wrapper library (see a separate pdf file)
 // In this file we repeated the calls of FabricJS methods
 
 // When we render <Canvas> FarbicJS canvas is created and initialized
-const canvas = new Canvas('canvas1', {
-  backgroundColor: '#F8F8F8',
+const canvas = new Canvas("canvas1", {
+  backgroundColor: "#F8F8F8",
   width: 800,
   height: 600,
 });
 
 // This is just for visualization (not part of problem :-)
-canvas.add(new Line([0, 100, 800, 100], { stroke: 'lightgrey', strokeWidth: 1 }));
-canvas.add(new Line([100, 0, 100, 600], { stroke: 'lightgrey', strokeWidth: 1 }));
+canvas.add(
+  new Line([0, 100, 800, 100], { stroke: "lightgrey", strokeWidth: 1 })
+);
+canvas.add(
+  new Line([100, 0, 100, 600], { stroke: "lightgrey", strokeWidth: 1 })
+);
 
-// <Group defaultOptions={{ x: 100, y: 100 }}> 
-const group1 = new Group([]); // group is created in useFarbicObject hook
+// <Group defaultOptions={{ x: 100, y: 100 }}>
+const group1 = new Group([], { left: 100, top: 100, layout: "fixed" }); // group is created in useFarbicObject hook
 
 // ExtendedCanvas._onObjectAdded (we redefine this method to update coordinates)
 canvas.add(group1); // add created object (that is - group1) to canvas
-group1.setXY(new Point(100, 100)); // update coordinates
-group1.setCoords();
-group1.triggerLayout();
-canvas.requestRenderAll();
+// group1.setXY(new Point(100, 100)); // update coordinates
+// group1.setCoords();
+// group1.triggerLayout();
+// canvas.requestRenderAll();
 
 // <Group defaultOptions={{ originX: 'center' }} >
-const group2 = new Group([], { originX: 'center' }); // group is created in useFarbicObject hook
+const group2 = new Group([], { originX: "center" }); // group is created in useFarbicObject hook
 
 // ExtendedGroup._onObjectAdded (and redefine method of Group as well)
-group1.triggerLayout();
+// group1.triggerLayout();
 group1.add(group2); // add created object (that is - group2) to group
-group2.setXY(new Point(100, 100));
-group2.setCoords();
-group2.triggerLayout();
+// group2.setXY(new Point(100, 100));
+// group2.setCoords();
+// group2.triggerLayout();
 canvas.requestRenderAll();
 
 // <Circle
@@ -49,19 +51,22 @@ canvas.requestRenderAll();
 //     originY: 'center'
 //   }}
 // />
-const circle1 = new Circle({ // circle is created in useFarbicObject hook
+const circle1 = new Circle({
+  // circle is created in useFarbicObject hook
   radius: 50,
-  fill: 'white',
-  shadow: '0 2 4 #d9d9d9',
-  originX: 'center',
-  originY: 'center'
+  fill: "white",
+  shadow: "0 2 4 #d9d9d9",
+  originX: "center",
+  originY: "center",
+  left: 100,
+  top: 100,
 });
 
 // ExtendedGroup._onObjectAdded
 group2.add(circle1);
-circle1.setXY(new Point(100, 100)); // we set absolute coordinates
-circle1.setCoords();
-group2.triggerLayout();
+// circle1.setXY(new Point(100, 100)); // we set absolute coordinates
+// circle1.setCoords();
+// group2.triggerLayout();
 canvas.requestRenderAll();
 
 // <Circle
@@ -77,14 +82,15 @@ canvas.requestRenderAll();
 //     originY: 'center'
 //   }}
 // />
-const circle2 = new Circle({ // circle is created in useFarbicObject hook
+const circle2 = new Circle({
+  // circle is created in useFarbicObject hook
   radius: 42,
-  fill: 'transparent',
-  stroke: '#E37566',
+  fill: "transparent",
+  stroke: "#E37566",
   strokeWidth: 6,
-  shadow: '0 2 4 #d9d9d9',
-  originX: 'center',
-  originY: 'center'
+  shadow: "0 2 4 #d9d9d9",
+  originX: "center",
+  originY: "center",
 });
 
 // ExtendedGroup._onObjectAdded
